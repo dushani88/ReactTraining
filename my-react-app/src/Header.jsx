e5db1a6fdc6a58182fd2b8c8c633c29d27c64779
Dushani Gunawardena <dushanigunawardena@Dushanis-MacBook-Air.local>
2025-07-22 21:22:26 +1000

    Added page elements

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,8 +1,26 @@
-function Header () {
-    return (
-        <header>
-            <h1>Hello world!</h1>
-        </header>
-    )
+function Header() {
+  return (
+    <div>
+      <header>
+        <img src="/react-logo.png" alt="Logo" width="40px" height="40px" />
+        <main>
+          <h1>Reasons I am exited to learn React</h1>
+          <ol>
+            <li>
+              React is a popular library, so I will be able to fit in with all
+              the coolest devs out there! 😎
+            </li>
+            <li>
+              I am more likely to get a job as a front end developer if I know
+              React
+            </li>
+          </ol>
+        </main>
+      </header>
+      <footer>
+        <small>© 2025 Dushan Gunawardena. All rights reserved.</small>
+      </footer>
+    </div>
+  );
 }
-export default Header
+export default Header;
